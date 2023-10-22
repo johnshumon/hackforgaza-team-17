d4ef1692bc7e0ad10cf7775399b98a57b32432bc
Yasir Idriz <yasiridriz@gmail.com>
2023-10-22 13:49:27 +0100

    incident point as a modal

---
+++ frontend/src/components/Menu.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import Vue from 'vue'
+
+</script>
+
+<template>
+    <div class="w-full flex ">
+
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
+
+import IncidentPoint from "../components/IncidentPoint.vue"
 </script>
 
 <template>
   <main>
-    
+
+    <IncidentPoint />
+
   </main>
 </template>
