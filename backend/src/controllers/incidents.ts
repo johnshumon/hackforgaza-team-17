779317c35ee1e56f0ba7f2331188da27411b2132
Tej Birring <tej@parkourops.com>
2023-10-22 10:29:27 +0100

    Future-proof /incidents endpoint.

--- backend/src/controllers/incidents.ts
+++ backend/src/controllers/incidents.ts
@@ -4,24 +4,22 @@ import neo4j from "../libs/neo4j";
 import { Incident } from "../../model/incident";
 
 const IncidentResult = z.object({
-    incident: Incident,
-    testimonies:  
+    incident: Incident
 });
+type IncidentResult = z.infer<typeof IncidentResult>;
 
 export const getIncidents = makeRouteHandler({
-    description: "Lists all incidents.",
+    description: "List incidents.",
     request: z.object({}),
     response: z.object({
-        result: Incident.array()
+        result: IncidentResult.array()
     }),
     query: z.object({
         limit: z.coerce.number().int().gt(0).optional(),
         page: z.coerce.number().int().gte(0).optional(),
     }),
     async handler(request, params, query) {
-        // 1. build query
-        
-        // 2. fetch data from databse
+        // 1. fetch data from database
         const graph = await neo4j.queryGraph({
             type: "read",
             text: `match (n:incident) return n`
@@ -34,19 +32,27 @@ export const getIncidents = makeRouteHandler({
             description: n.properties.description,
             tags: n.properties.tags
         }));
-        // 2. parse
+        // 2. parse fetched data
+        // - parse incidents
         const parseResult = Incident.array().safeParse(rawIncidents);
         if (!parseResult.success) {
-            return INTERNAL_SERVER_ERROR_RESPONSE("Failed to parse result.");
+            return INTERNAL_SERVER_ERROR_RESPONSE("Failed to parse incident.");
         }
         const parsedIncidents = parseResult.data;
-        // 3. 
+        // 3. transform fetched data for result
+        const aggregateResult = parsedIncidents.map((i)=>{
+            const result : IncidentResult = {
+                incident: i
+            }
+            return result
+        });
+        // 4. return response
         return {
             status: 200,
             userFriendlyMessage: parsedIncidents.length > 0 ? "Incidents found." : "No incidents found.",
             data: {
-                result: parsedIncidents
+                result: aggregateResult
             }
         }        
     },
-}) 
+})
