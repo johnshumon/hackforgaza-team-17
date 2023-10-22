2b8c4dc62a0b07d2d38b5b235883764438658d85
Tej Birring <tej@parkourops.com>
2023-10-22 20:20:22 +0100

    controller can act as mock server (test-data generator).

--- backend/src/controllers/incidents.ts
+++ backend/src/controllers/incidents.ts
@@ -1,9 +1,10 @@
 import { z } from "zod";
-import { makeRouteHandler, INTERNAL_SERVER_ERROR_RESPONSE } from "../framework";
+import { makeRouteHandler } from "../framework";
 import neo4j from "../libs/neo4j";
 import { Incident } from "../../model/incident";
+import { generateRandomIncidents } from "./random-generators/incidents-palestine";
 
-const IncidentResult = z.object({
+export const IncidentWithContext = z.object({
     incident: Incident,
     victims: z.object({
         killed: z.number().int().gte(0),
@@ -13,53 +14,74 @@ const IncidentResult = z.object({
         dispossessed: z.number().int().gte(0),
     })
 });
-type IncidentResult = z.infer<typeof IncidentResult>;
+export type IncidentWithContext = z.infer<typeof IncidentWithContext>;
+
+export const GetIncidentResult = IncidentWithContext.array();
+export type GetIncidentResult = z.infer<typeof GetIncidentResult>;
+
+async function getIncidentsFromDatabase() : Promise<GetIncidentResult> {
+    // 1. fetch data from database
+    const graph = await neo4j.queryGraph({
+        type: "read",
+        text: `match (n:incident) return n`
+    });
+    const rawIncidents = graph.nodes.map((n)=>({
+        id: n.id,
+        title: n.properties.title,
+        dateTime: n.properties.dateTime,
+        location: n.properties.location,
+        description: n.properties.description,
+        tags: n.properties.tags
+    }));
+    // 2. parse fetched data
+    // - parse incidents
+    const parseResult = Incident.array().safeParse(rawIncidents);
+    if (!parseResult.success) {
+        return [];
+    }
+    const parsedIncidents = parseResult.data;
+    // 3. transform fetched data for result
+    const aggregateResult = parsedIncidents.map((i)=>{
+        const result : IncidentWithContext = {
+            incident: i,
+            victims: {
+                killed: 0,
+                maimed: 0,
+                injured: 0,
+                detained: 0,
+                dispossessed: 0
+            }
+        }
+        return result
+    });
+    return aggregateResult;
+} 
 
 export const getIncidents = makeRouteHandler({
     description: "List incidents.",
     request: z.object({}),
     response: z.object({
-        result: IncidentResult.array()
+        result: GetIncidentResult
     }),
     query: z.object({
+        random: z.coerce.number().int().gt(0).optional(),
         limit: z.coerce.number().int().gt(0).optional(),
         page: z.coerce.number().int().gte(0).optional(),
     }),
     async handler(request, params, query) {
-        // 1. fetch data from database
-        const graph = await neo4j.queryGraph({
-            type: "read",
-            text: `match (n:incident) return n`
-        });
-        const rawIncidents = graph.nodes.map((n)=>({
-            id: n.id,
-            title: n.properties.title,
-            dateTime: n.properties.dateTime,
-            location: n.properties.location,
-            description: n.properties.description,
-            tags: n.properties.tags
-        }));
-        // 2. parse fetched data
-        // - parse incidents
-        const parseResult = Incident.array().safeParse(rawIncidents);
-        if (!parseResult.success) {
-            return INTERNAL_SERVER_ERROR_RESPONSE("Failed to parse incident.");
+        let incidents : GetIncidentResult = [];
+        if (query?.random) {
+            incidents = generateRandomIncidents(query.random);
+        } else {
+            incidents = await getIncidentsFromDatabase();
         }
-        const parsedIncidents = parseResult.data;
-        // 3. transform fetched data for result
-        const aggregateResult = parsedIncidents.map((i)=>{
-            const result : IncidentResult = {
-                incident: i
-            }
-            return result
-        });
-        // 4. return response
+
         return {
             status: 200,
-            userFriendlyMessage: parsedIncidents.length > 0 ? "Incidents found." : "No incidents found.",
+            userFriendlyMessage: incidents.length > 0 ? "Incidents found." : "No incidents found.",
             data: {
-                result: aggregateResult
+                result: incidents
             }
-        }        
-    },
+        }
+    }
 })
