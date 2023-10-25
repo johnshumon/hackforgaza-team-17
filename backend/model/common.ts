a11ca4e7d3d1a43e1000b968d9bae9da0546739f
Tej Birring <tej@parkourops.com>
2023-10-25 13:23:55 +0100

    Make count mandatory.

--- backend/model/common.ts
+++ backend/model/common.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const NonEmptyText = z.string().min(1);
 export const MaybeEmptyText = z.string();
-export const Count = z.number().int().gte(0).optional();
+export const Count = z.number().int().gte(0);
 export const AuditStatus = z.union([
     z.literal("audited:verified"),
     z.literal("audited:unreliable"),
